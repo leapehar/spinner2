74442b2b8f6bdc83fbe795e08e8266a86c9d6b9c
Lea Pehar <leapehar@gmail.com>
2022-01-18 21:29:51 +0000

    made changes to the spinner so that it makes 2 full rotations instead of just one

--- spinner1.js
+++ spinner1.js
@@ -18,10 +18,27 @@ setTimeout(() => {
   process.stdout.write('\r\\\    ');
 }, 700);
 
+setTimeout(() => {
 
-// ... fill in the rest yourself ...
+  process.stdout.write('\r|  ');
+}, 900);
+
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+
+  process.stdout.write('\r\\\    ');
+}, 1500);
+
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
   process.stdout.write('\r|  ');
-}, 900);
+}, 1700);
